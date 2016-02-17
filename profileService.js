10da0f998402a4d36fd1914675c58dbfcd8d5c2b
Breiden Busch <breiden.busch@gmail.com>
2016-02-17 12:29:05 -0700

    Finished Day 3 Step 2

--- profileService.js
+++ profileService.js
@@ -1,17 +1,27 @@
 angular.module('devMtIn')
 
-myApp.service('profileService', function() {
+myApp.service('profileService', function($http) {
+
+  var baseUrl = "http://connections.devmounta.in/";
 
   this.saveProfile = function(profile) {
-    localStorage.setItem('profile', JSON.stringify(profile));
+    return $http({
+      method: 'POST',
+      url: baseUrl + 'api/profiles/',
+      data: profile
+    })
+    .then(function(profileResponse) {
+        console.log(profileResponse);
+        localStorage.setItem('profileId', JSON.stringify({ profileId: profileResponse.data._id }));
+    })
+    .catch(function(err) {
+        console.log(err);
+})
   }
 
   this.checkForProfile = function() {
     if (localStorage.getItem('profile')) {
       return JSON.parse(localStorage.getItem('profile'));
-    }
-    return {
-      friends: [{name: 'Ryan'}, {name: 'Bryan'}, {name: 'Sarah'}, {name: 'Zac'}, {name: 'Erin'}]
     }
   }
 
