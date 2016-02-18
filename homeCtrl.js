ecf17ab3aa0d800d7d8b5a68654fd00b1e2be4d7
Breiden Busch <breiden.busch@gmail.com>
2016-02-18 15:06:11 -0700

    Finished Project, Working on Minor Bugs

--- homeCtrl.js
+++ homeCtrl.js
@@ -1,6 +1,6 @@
 angular.module('devMtIn');
 
-myApp.controller('homeCtrl', function($scope, profileService) {
+myApp.controller('homeCtrl', function($scope, profileService, friendService) {
 
   $scope.editing = false;
 
@@ -16,12 +16,14 @@ myApp.controller('homeCtrl', function($scope, profileService) {
       profileService.checkForProfile(profileId.profileId)
       .then(function(profile) {
         $scope.myProfile = profile.data;
+        friendService.findFriendsFriends(profile.data); // Finding second level friends.
       })
       .catch(function(err) {
-        console.log(err);
+        console.error(err);
       });
     }
   }
+  
   $scope.checkForProfile();
 
   $scope.sortOptions = [{
@@ -43,6 +45,24 @@ myApp.controller('homeCtrl', function($scope, profileService) {
       .catch(function(err) {
         console.log(err);
       });
+    }
+
+    $scope.findFriends = function(query) {
+      friendService.findFriends($scope.myProfile._id, query).then(function(response) {
+        $scope.potentialFriends = response.data;
+      })
+    }
+
+    $scope.addFriend = function(friendId) {
+      friendService.addFriend($scope.myProfile._id, friendId).then(function() {
+        $scope.checkForProfile();
+      })
+    }
+
+    $scope.removeFriend = function(friendId) {
+      friendService.removeFriend($scope.myProfile._id, friendId).then(function() {
+        $scope.checkForProfile();
+      })
     }
 
 });
